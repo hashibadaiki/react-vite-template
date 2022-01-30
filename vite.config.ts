035392b2a6b876f248d69d38d35bdc7b6d8955c4
hashibadaiki <higemeganeking@gmail.com>
2022-01-30 11:12:01 +0900

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  // dev server起動でwindow open
+  server: {
+    open: true,
+  },
+  root: "./src",
+  build: {
+    // root (= ./src) から見た相対パスで指定
+    outDir: "../public",
+    // git hub pages用
+    // outDir: '../docs',
+    emptyOutDir: true,
+  },
+  plugins: [react()],
+});
