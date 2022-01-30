3b1b0fa26193e6ca25647d3d63706db20b8f8dc7
hashibadaiki <higemeganeking@gmail.com>
2022-01-30 11:18:01 +0900

    add service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { registerSW } from 'virtual:pwa-register'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+registerSW();
