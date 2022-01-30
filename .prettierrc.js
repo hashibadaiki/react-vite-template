697a68b8d8f903a2eb346bac5c8a1c6bcf14fdd4
hashibadaiki <higemeganeking@gmail.com>
2022-01-30 15:50:37 +0900

    fix prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // デフォルト設定も含みますがバージョンチェンジで変動することがあるので明示している
+  // https://qiita.com/takeshisakuma/items/bbb2cd2f1c65de70e363#javascript%E3%81%AE%E5%A0%B4%E5%90%88
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  parser: "none",
+  filepath: "none",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "auto",
+  embeddedLanguageFormatting: "auto",
+};
