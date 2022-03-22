86dc5dec49d62eb4e02e8a07110130823e5b1d79
hashibadaiki <higemeganeking@gmail.com>
2022-03-22 20:42:09 +0900

    eslint をviteでもwatchするように変更した

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,5 +25,11 @@ export default defineConfig({
       },
     }),
     VitePWA(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint . --ext .js,.jsx,.ts,.tsx',
+      },
+    }),
+    checker({ typescript: true }),
   ],
 });
