700128fdf7f7be52c116cbb2c446490af57500e0
hashibadaiki <higemeganeking@gmail.com>
2023-07-28 13:35:16 +0900

    add: modules

--- src/components/atoms/Atom.tsx
+++ src/components/atoms/Atom.tsx
@@ -1,9 +1,10 @@
+import styles from './atom.module.scss';
 const test = 'test';
 
 export const Atom = () => {
   return (
     <div>
-      <div>Atom</div>
+      <div className={styles.color}>Atom</div>
       <div>{test}</div>
     </div>
   );
