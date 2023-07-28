01a7429211b704062e51b10581aca76bf5923b85
hashibadaiki <higemeganeking@gmail.com>
2023-07-28 13:28:17 +0900

    不要なコード削除した

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from 'react-dom/client';
-// import { registerSW } from 'virtual:pwa-register';
 import { App } from './pages/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
 root.render(<App />);
-
-// registerSW();
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,9 @@
 import { Template } from '../components/templates/Template';
-import { css } from '@emotion/react';
-
-const sampleStyle = css`
-  width: 100%;
-  color: red;
-`;
 
 export const App = () => {
   return (
     <div>
-      <div css={sampleStyle}>App</div>
+      <div>App</div>
       <Template />
     </div>
   );
